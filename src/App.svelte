46f86e90f56a33d4bc773c8f2b0402c5cadda996
Alexander Shvaykin <skiline.alex@gmail.com>
2021-03-31 23:20:28 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { verbs } from "./verbs"
+  let q;
+  function findWord(name, word) {
+    return word.simple.includes(name) || word.participle.includes(name) || word.rus.includes(name)
+  }
+</script>
+
+<input type="text" bind:value={q}>
+<ul>
+  {#each verbs.filter(w => findWord(q, w)) as word}
+    <li>{word.simple} {word.participle} {word.rus}</li>
+  {/each}
+</ul>
---
+++ src/verbs.js
@@ -0,0 +1,602 @@
+export const verbs = [
+  {
+    "simple": "arise",
+    "participle": "arose, arisen",
+    "rus": "подниматься"
+  },
+  {
+    "simple": "awake",
+    "participle": "awoke, awoken",
+    "rus": "пробуждать"
+  },
+  {
+    "simple": "be",
+    "participle": "was (were)",
+    "rus": "быть"
+  },
+  {
+    "simple": "bear",
+    "participle": "bore, born",
+    "rus": "переносить"
+  },
+  {
+    "simple": "beat",
+    "participle": "beat, beaten",
+    "rus": "бить"
+  },
+  {
+    "simple": "become",
+    "participle": "became, become",
+    "rus": "становиться"
+  },
+  {
+    "simple": "begin",
+    "participle": "began, begun",
+    "rus": "начинать"
+  },
+  {
+    "simple": "bend",
+    "participle": "bent, bent",
+    "rus": "наклонять"
+  },
+  {
+    "simple": "bet",
+    "participle": "bet, bet",
+    "rus": "пари"
+  },
+  {
+    "simple": "bind",
+    "participle": "bound, bound",
+    "rus": "связывать"
+  },
+  {
+    "simple": "bite",
+    "participle": "bit, bitten",
+    "rus": "кусать"
+  },
+  {
+    "simple": "bleed",
+    "participle": "bled, bled",
+    "rus": "кровоточить"
+  },
+  {
+    "simple": "blow",
+    "participle": "blew, blown",
+    "rus": "дуть"
+  },
+  {
+    "simple": "break",
+    "participle": "broke, broken",
+    "rus": "ломать"
+  },
+  {
+    "simple": "breed",
+    "participle": "bred, bred",
+    "rus": "животных"
+  },
+  {
+    "simple": "bring",
+    "participle": "brought, brought",
+    "rus": "приносить"
+  },
+  {
+    "simple": "build",
+    "participle": "built, built",
+    "rus": "строить"
+  },
+  {
+    "simple": "buy",
+    "participle": "bought, bought",
+    "rus": "покупать"
+  },
+  {
+    "simple": "catch",
+    "participle": "caught, caught",
+    "rus": "ловить"
+  },
+  {
+    "simple": "choose",
+    "participle": "chose, chosen",
+    "rus": "выбирать"
+  },
+  {
+    "simple": "cling",
+    "participle": "clung, clung",
+    "rus": "цепляться"
+  },
+  {
+    "simple": "come",
+    "participle": "came, come",
+    "rus": "приходить"
+  },
+  {
+    "simple": "cost",
+    "participle": "cost, cost",
+    "rus": "стоить"
+  },
+  {
+    "simple": "cut",
+    "participle": "cut, cut",
+    "rus": "резать"
+  },
+  {
+    "simple": "deal",
+    "participle": "dealt, dealt",
+    "rus": "дела"
+  },
+  {
+    "simple": "dig",
+    "participle": "dug, dug",
+    "rus": "копать"
+  },
+  {
+    "simple": "do",
+    "participle": "did, done",
+    "rus": "делать"
+  },
+  {
+    "simple": "draw",
+    "participle": "drew, drawn",
+    "rus": "тянуть"
+  },
+  {
+    "simple": "drink",
+    "participle": "drank, drunk",
+    "rus": "пить"
+  },
+  {
+    "simple": "drive",
+    "participle": "drove, driven",
+    "rus": "автомобиль"
+  },
+  {
+    "simple": "eat",
+    "participle": "ate, eaten",
+    "rus": "кушать"
+  },
+  {
+    "simple": "fall",
+    "participle": "fell, fallen",
+    "rus": "падать"
+  },
+  {
+    "simple": "feed",
+    "participle": "fed, fed",
+    "rus": "кормить"
+  },
+  {
+    "simple": "feel",
+    "participle": "felt, felt",
+    "rus": "чувствовать"
+  },
+  {
+    "simple": "fight",
+    "participle": "fought, fought",
+    "rus": "бороться"
+  },
+  {
+    "simple": "find",
+    "participle": "found, found",
+    "rus": "находить"
+  },
+  {
+    "simple": "flee",
+    "participle": "fled, fled",
+    "rus": "сбегать"
+  },
+  {
+    "simple": "fly",
+    "participle": "flew, flown",
+    "rus": "летать"
+  },
+  {
+    "simple": "forbid",
+    "participle": "forbade, forbidden",
+    "rus": "запрещать"
+  },
+  {
+    "simple": "forget",
+    "participle": "forgot, forgotten",
+    "rus": "забывать"
+  },
+  {
+    "simple": "forgive",
+    "participle": "forgave, forgiven",
+    "rus": "прощать"
+  },
+  {
+    "simple": "freeze",
+    "participle": "froze, frozen",
+    "rus": "замораживать"
+  },
+  {
+    "simple": "get",
+    "participle": "got, got (gotten)",
+    "rus": "получать"
+  },
+  {
+    "simple": "give",
+    "participle": "gave, given",
+    "rus": "давать"
+  },
+  {
+    "simple": "go",
+    "participle": "went, gone",
+    "rus": "идти"
+  },
+  {
+    "simple": "grow",
+    "participle": "grew, grown",
+    "rus": "расти"
+  },
+  {
+    "simple": "hang",
+    "participle": "hung, hung",
+    "rus": "висеть"
+  },
+  {
+    "simple": "have",
+    "participle": "had, had",
+    "rus": "иметь"
+  },
+  {
+    "simple": "hear",
+    "participle": "heard, heard",
+    "rus": "слышать"
+  },
+  {
+    "simple": "hide",
+    "participle": "hid, hidden",
+    "rus": "прятать"
+  },
+  {
+    "simple": "hit",
+    "participle": "hit, hit",
+    "rus": "попадать"
+  },
+  {
+    "simple": "hold",
+    "participle": "held, held",
+    "rus": "держать"
+  },
+  {
+    "simple": "hurt",
+    "participle": "hurt, hurt",
+    "rus": "боль"
+  },
+  {
+    "simple": "keep",
+    "participle": "kept, kept",
+    "rus": "соблюдать"
+  },
+  {
+    "simple": "know",
+    "participle": "knew, known",
+    "rus": "знать"
+  },
+  {
+    "simple": "lay",
+    "participle": "laid, laid",
+    "rus": "класть"
+  },
+  {
+    "simple": "lead",
+    "participle": "led, led",
+    "rus": "лидировать"
+  },
+  {
+    "simple": "learn",
+    "participle": "learnt (learned), learnt (learned)",
+    "rus": "узнавать"
+  },
+  {
+    "simple": "leave",
+    "participle": "left, left",
+    "rus": "оставлять"
+  },
+  {
+    "simple": "lend",
+    "participle": "lent, lent",
+    "rus": "взаймы"
+  },
+  {
+    "simple": "let",
+    "participle": "let, let",
+    "rus": "позволять"
+  },
+  {
+    "simple": "lie",
+    "participle": "lay, lain",
+    "rus": "лежать"
+  },
+  {
+    "simple": "light",
+    "participle": "lit, lit",
+    "rus": "освещать"
+  },
+  {
+    "simple": "lose",
+    "participle": "lost, lost",
+    "rus": "терять"
+  },
+  {
+    "simple": "make",
+    "participle": "made, made",
+    "rus": "мастерить"
+  },
+  {
+    "simple": "mean",
+    "participle": "meant, meant",
+    "rus": "значить"
+  },
+  {
+    "simple": "meet",
+    "participle": "met, met",
+    "rus": "знакомиться"
+  },
+  {
+    "simple": "pay",
+    "participle": "paid, paid",
+    "rus": "платить"
+  },
+  {
+    "simple": "put",
+    "participle": "put, put",
+    "rus": "ставить"
+  },
+  {
+    "simple": "read",
+    "participle": "read, read",
+    "rus": "читать"
+  },
+  {
+    "simple": "ride",
+    "participle": "rode, ridden",
+    "rus": "верхом"
+  },
+  {
+    "simple": "ring",
+    "participle": "rang, rung",
+    "rus": "звонить"
+  },
+  {
+    "simple": "rise",
+    "participle": "rose, risen",
+    "rus": "подниматься"
+  },
+  {
+    "simple": "run",
+    "participle": "ran, run",
+    "rus": "бежать"
+  },
+  {
+    "simple": "say",
+    "participle": "said, said",
+    "rus": "сказать"
+  },
+  {
+    "simple": "see",
+    "participle": "saw, seen",
+    "rus": "видеть"
+  },
+  {
+    "simple": "seek",
+    "participle": "sought, sought",
+    "rus": "искать"
+  },
+  {
+    "simple": "sell",
+    "participle": "sold, sold",
+    "rus": "продавать"
+  },
+  {
+    "simple": "send",
+    "participle": "sent, sent",
+    "rus": "посылать"
+  },
+  {
+    "simple": "set",
+    "participle": "set, set",
+    "rus": "устанавливать"
+  },
+  {
+    "simple": "shake",
+    "participle": "shook, shaken",
+    "rus": "трясти"
+  },
+  {
+    "simple": "shine",
+    "participle": "shone, shone",
+    "rus": "сиять"
+  },
+  {
+    "simple": "shoot",
+    "participle": "shot, shot",
+    "rus": "стрелять"
+  },
+  {
+    "simple": "show",
+    "participle": "showed, shown",
+    "rus": "показывать"
+  },
+  {
+    "simple": "shrink",
+    "participle": "shrank, shrunk",
+    "rus": "сжиматься"
+  },
+  {
+    "simple": "shut",
+    "participle": "shut, shut",
+    "rus": "затворять"
+  },
+  {
+    "simple": "sing",
+    "participle": "sang, sung",
+    "rus": "петь"
+  },
+  {
+    "simple": "sit",
+    "participle": "sat, sat",
+    "rus": "сидеть"
+  },
+  {
+    "simple": "sleep",
+    "participle": "slept, slept",
+    "rus": "спать"
+  },
+  {
+    "simple": "slide",
+    "participle": "slid, slid",
+    "rus": "скользить"
+  },
+  {
+    "simple": "smell",
+    "participle": "smelt, smelt",
+    "rus": "нюхать"
+  },
+  {
+    "simple": "speak",
+    "participle": "spoke, spoken",
+    "rus": "говорить"
+  },
+  {
+    "simple": "spell",
+    "participle": "spelt (spelled), spelt (spelled)",
+    "rus": "буквам"
+  },
+  {
+    "simple": "spend",
+    "participle": "spent, spent",
+    "rus": "время"
+  },
+  {
+    "simple": "spill",
+    "participle": "spilt (spilled), spilt (spilled)",
+    "rus": "разлить"
+  },
+  {
+    "simple": "spin",
+    "participle": "spun, spun",
+    "rus": "крутить"
+  },
+  {
+    "simple": "split",
+    "participle": "split, split",
+    "rus": "раскалывать"
+  },
+  {
+    "simple": "spoil",
+    "participle": "spoilt (spoiled), spoilt (spoiled)",
+    "rus": "портить"
+  },
+  {
+    "simple": "spread",
+    "participle": "spread, spread",
+    "rus": "распространять"
+  },
+  {
+    "simple": "stand",
+    "participle": "stood, stood",
+    "rus": "стоять"
+  },
+  {
+    "simple": "steal",
+    "participle": "stole, stolen",
+    "rus": "воровать"
+  },
+  {
+    "simple": "sting",
+    "participle": "stung, stung",
+    "rus": "жалить"
+  },
+  {
+    "simple": "stink",
+    "participle": "stank, stunk",
+    "rus": "вонять"
+  },
+  {
+    "simple": "strike",
+    "participle": "struck, struck",
+    "rus": "ударять"
+  },
+  {
+    "simple": "swear",
+    "participle": "swore, sworn",
+    "rus": "ругаться"
+  },
+  {
+    "simple": "sweep",
+    "participle": "swept, swept",
+    "rus": "подметать"
+  },
+  {
+    "simple": "swell",
+    "participle": "swelled, swollen (swelled)",
+    "rus": "опухать"
+  },
+  {
+    "simple": "swim",
+    "participle": "swam, swum",
+    "rus": "плавать"
+  },
+  {
+    "simple": "take",
+    "participle": "took, taken",
+    "rus": "брать"
+  },
+  {
+    "simple": "teach",
+    "participle": "taught, taught",
+    "rus": "обучать"
+  },
+  {
+    "simple": "tear",
+    "participle": "tore, torn",
+    "rus": "рвать"
+  },
+  {
+    "simple": "tell",
+    "participle": "told, told",
+    "rus": "рассказывать"
+  },
+  {
+    "simple": "think",
+    "participle": "thought, thought",
+    "rus": "думать"
+  },
+  {
+    "simple": "throw",
+    "participle": "threw, thrown",
+    "rus": "бросать"
+  },
+  {
+    "simple": "understand",
+    "participle": "understood, understood",
+    "rus": "понимать"
+  },
+  {
+    "simple": "wake",
+    "participle": "woke, woken",
+    "rus": "будить"
+  },
+  {
+    "simple": "wear",
+    "participle": "wore, worn",
+    "rus": ""
+  },
+  {
+    "simple": "win",
+    "participle": "won, won",
+    "rus": "побеждать"
+  },
+  {
+    "simple": "wind",
+    "participle": "wound, wound",
+    "rus": "изгибаться"
+  },
+  {
+    "simple": "write",
+    "participle": "wrote, written",
+    "rus": "писать"
+  }
+]
