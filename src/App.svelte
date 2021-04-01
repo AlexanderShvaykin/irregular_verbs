7c1cebc20fad42bdd8347369c8c78331f6fd9067
Alexander Shvaykin <skiline.alex@gmail.com>
2021-04-01 09:47:18 +0300

    Adds styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,33 @@
 <script>
   import { verbs } from "./verbs"
-  let q;
+  let q = "";
   function findWord(name, word) {
     return word.simple.includes(name) || word.participle.includes(name) || word.rus.includes(name)
   }
 </script>
 
-<input type="text" bind:value={q}>
-<ul>
+<style>
+  .list {
+      width: 535px;
+      margin: auto;
+  }
+  .list p span {
+      margin-right: 10px;
+      display: inline-block;
+      word-wrap: break-word;
+      text-align: left;
+  }
+  .list p span.w1 {
+      width: 100px;
+  }
+  .list p span.w2 {
+      width: 200px;
+  }
+</style>
+
+<div class="list">
+  <input type="text" bind:value={q}>
   {#each verbs.filter(w => findWord(q, w)) as word}
-    <li>{word.simple} {word.participle} {word.rus}</li>
+    <p><span class="w1">{word.simple}</span><span class="w2">{word.participle}</span><span class="w2">{word.rus}</span></p>
   {/each}
-</ul>
+</div>
